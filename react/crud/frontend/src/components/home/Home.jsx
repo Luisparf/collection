dc6a0fb223e6920dc0e6a0610db6d6749ec34f34
Luisparf <luispauloaraujoafonso@gmail.com>
2022-03-24 16:25:35 -0300

    envolving..

---
+++ react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default probs =>
+        <Main icon="home" tittle="Início" subtittle="Development">
+            <div className='display-4'>Bem vindo!</div>
+            <hr/>
+            <p className='mp-o'>Create Read Update Delete</p>
+        </Main>
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -4,13 +4,13 @@ import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props => 
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" tittle="Home" subtittle="Development"/>
+        <Home />
         <Footer />
     </div>
